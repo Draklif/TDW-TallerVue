d50994f9fb73c93278f9054138376228d448d666
Danklif <josemanrenteria@gmail.com>
2023-09-14 11:56:19 -0500

    140923-02 Estilos finales

--- src/App.vue
+++ src/App.vue
@@ -1,146 +1,192 @@
 <script setup lang="ts">
-  import { ref, reactive } from 'vue'
-  import Table from './components/Table.vue'
+import { ref, reactive } from 'vue'
+import Table from './components/Table.vue'
 
-  const newArticle = ref('');
-  const newQuantity = ref(1);
-  const newValue = ref(1);
+const newArticle = ref('')
+const newQuantity = ref(1)
+const newValue = ref(1)
 
-  const quantityTotal = ref(0);
-  const totalBase = ref(0);
-  const totalAbsolute = ref(0);
-  const discount = ref(0);
+const totalBase = ref(0)
+const totalAbsolute = ref(0)
+const discount = ref(0)
 
-  type Item = {
-    article: string,
-    quantity: number,
-    value: number
+// Se define el tipo Item con las propiedades que tomará siempre
+type Item = {
+  article: string
+  quantity: number
+  value: number
+}
+
+// Se define un arreglo reactivo que toma únicamente arreglos de tipo Item
+const items = reactive<Item[]>([])
+
+const addItem = () => {
+  if (newArticle.value === '') {
+    alert('Ingrese un nombre para su artículo')
+    return
   }
 
-  const items = reactive<Item[]>([])
-
-  const addItem = () => {
-    if (newArticle.value === '') {
-      alert('Ingrese un nombre para su artículo')
-      return
-    }
-
-    if (newQuantity.value <= 0 || newValue.value <= 0) {
-      alert('Ingrese únicamente valores positivos diferentes de 0')
-      return
-    }
-
-    items.push({
-      article: newArticle.value,
-      quantity: newQuantity.value,
-      value: newValue.value
-    })
-
-    calculatePrice()
-
-    newArticle.value = ''
-    newQuantity.value = 1
-    newValue.value = 1
+  if (newQuantity.value <= 0 || newValue.value <= 0) {
+    alert('Ingrese únicamente valores positivos diferentes de 0')
+    return
   }
 
-  const onItemDeleted = () => {
-    calculatePrice()
+  items.push({
+    article: newArticle.value,
+    quantity: newQuantity.value,
+    value: newValue.value
+  })
 
-    newArticle.value = ''
-    newQuantity.value = 1
-    newValue.value = 1
+  calculatePrice()
+  resetForm()
+
+  newArticle.value = ''
+  newQuantity.value = 1
+  newValue.value = 1
+}
+
+const onItemDeleted = () => {
+  calculatePrice()
+  resetForm()
+}
+
+const calculatePrice = () => {
+  // El método 'reduce' se encarga de reducir un array a un solo valor, acumulando los elementos a un solo valor
+  // Funciona como un for en el sentido que itera todo el array y, en cada iteración, toma el valor de la propiedad 'quantity' y la suma al acumulador llamado 'acum'. El 0 es el valor inicial de 'acum'.
+  let quantityTotal = items.reduce((acum, item) => acum + item.quantity, 0)
+  totalBase.value = items.reduce((acum, item) => acum + item.value * item.quantity, 0)
+
+  discount.value = 0
+
+  if (totalBase.value >= 60000) {
+    discount.value = 5
+  }
+  if (totalBase.value >= 120000) {
+    discount.value = 10
+  }
+  if (totalBase.value >= 240000) {
+    discount.value = 15
   }
 
-  const calculatePrice = () => {
-    quantityTotal.value = 0
-    totalBase.value = 0
-    discount.value = 0
-    totalAbsolute.value = 0
-    for (const item of items) {
-      quantityTotal.value = quantityTotal.value + item.quantity
-      totalBase.value = totalBase.value + (item.value * item.quantity)
-
-      if (totalBase.value >= 60000 && discount.value < 5) {
-        discount.value = 5
-      } else if (totalBase.value >= 120000 && discount.value < 10) {
-        discount.value = 10
-      } else if (totalBase.value >= 240000 && discount.value < 15) {
-        discount.value = 15
-      }
-
-      if (quantityTotal.value >= 6 && discount.value < 10) {
-        discount.value = 10
-      } else if (quantityTotal.value >= 12 && discount.value < 20) {
-        discount.value = 20
-      }
-
-      discount.value = quantityTotal.value < 6 && totalBase.value < 60000 ? 0 : discount.value 
-      totalAbsolute.value = Math.floor(totalBase.value * (1 - (discount.value / 100)))
-    } 
+  if (quantityTotal >= 12) {
+    discount.value = 20
+  } else if (quantityTotal >= 6) {
+    discount.value = 10
   }
+
+  totalAbsolute.value = Math.floor(totalBase.value * (1 - discount.value / 100))
+}
+
+const resetForm = () => {
+  newArticle.value = ''
+  newQuantity.value = 1
+  newValue.value = 1
+}
 </script>
 
 <template>
-  <div class="formContainer">
-    <form style="display: flex; flex-direction: column;" @submit.prevent="addItem">
-      <label for="txtArticle">Artículo</label>
-      <input id="txtArticle" type="text" v-model="newArticle">
-      <label for="txtQuantity">Cantidad</label>
-      <input id="txtQuantity" type="number" v-model="newQuantity">
-      <label for="txtValue">Valor Unitario</label>
-      <input id="txtValue" type="number" v-model="newValue">
-      <button type="submit">Agregar</button>
-    </form>
-  </div>
+  <form @submit.prevent="addItem" class="form">
+    <label for="txtArticle" class="label">Artículo</label>
+    <input id="txtArticle" type="text" v-model="newArticle" class="input" />
+    <label for="txtQuantity" class="label">Cantidad</label>
+    <input id="txtQuantity" type="number" v-model="newQuantity" class="input" />
+    <label for="txtValue" class="label">Valor Unitario</label>
+    <input id="txtValue" type="number" v-model="newValue" class="input" />
+    <button type="submit" class="button">Agregar</button>
+  </form>
 
-  <div class="dataContainer">
-    <Table :items="items" @item-deleted="onItemDeleted"/>
+  <section class="dataContainer">
+    <Table :items="items" @item-deleted="onItemDeleted" />
 
     <div class="showTotal">
-      <label for="totalBase">Total compra: </label>
-      <p id="totalBase" style="display: inline;">${{ totalBase }}</p>
-      <br>
-      <label for="discount">Descuento: </label>
-      <p id="discount" style="display: inline;">{{ discount }}%</p>
-      <br>
-      <label for="total">Total a pagar: </label>
-      <p id="total" style="display: inline;">${{ totalAbsolute }}</p>
+      <label for="totalBase" class="totalLabel">Total compra:</label>
+      <p id="totalBase" class="totalValue">${{ totalBase }}</p>
+      <br />
+      <label for="discount" class="totalLabel">Descuento:</label>
+      <p id="discount" class="totalValue">{{ discount }}%</p>
+      <br />
+      <label for="total" class="totalLabel">Total a pagar:</label>
+      <p id="total" class="totalValue">${{ totalAbsolute }}</p>
     </div>
-  </div>
+  </section>
 </template>
 
 <style scoped>
-template {
-  display: flex;
-  flex-direction: row;
-}
 
-.formContainer {
+.form {
+  color: black;
   display: flex;
-  justify-content: center;
-  color: white;
-  border-radius: 16px;
-  background-color: rgb(44, 44, 44);
+  flex-direction: column;
+  align-items: center;
   padding: 16px;
-  margin: 16px;
+  background-color: #f0f0f0;
+  border-radius: 8px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+  width: calc(100% - 256px);
+  margin-right: 32px;
+  justify-self: end;
+  align-self: center;
 }
 
-button {
-  width: 256px;
-  margin-top: 16px;
-  border: 0;
-  border-radius: 16px;
+.label {
+  font-size: 16px;
+  margin-bottom: 4px;
+}
+
+.input {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-bottom: 12px;
+}
+
+.button {
+  width: 100%;
+  padding: 12px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+}
+
+.button:hover {
+  background-color: #0056b3;
+}
+
+.totalLabel {
+  font-size: 18px;
+  margin-bottom: 4px;
+}
+
+.totalValue {
+  font-size: 20px;
+  font-weight: bold;
+  color: #007bff;
 }
 
 .dataContainer {
   display: flex;
   flex-direction: column;
-  margin: 16px;
+  align-items: center;
+  padding: 16px;
+  background-color: #f0f0f0;
+  border-radius: 8px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+  color: black;
+  width: calc(600px);
+  margin-right: 32px;
+  justify-self: start;
+  overflow-y: auto;
+  max-height: 80vh;
 }
 
-.showTotal {  
+.showTotal {
   margin-top: 24px;
-  align-self: end;
+  align-self: flex-end;
 }
-
 </style>
+
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -13,7 +13,7 @@
 </script>
 
 <template>
-  <table>
+  <table class="table">
     <thead>
       <tr>
         <th>#</th>
@@ -30,11 +30,43 @@
         <td>{{ item.quantity }}</td>
         <td>${{ item.value }}</td>
         <td>${{ item.quantity * item.value }}</td>
-        <td><button type="button" @click="deleteItem(index)"><DeleteIcon /></button></td>
+        <td><button type="button" @click="deleteItem(index)" class="deleteButton"><DeleteIcon /></button></td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <style scoped>
+.table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+  overflow-y: auto;
+  max-height: 320px;
+}
+
+.table th,
+.table td {
+  padding: 10px;
+  text-align: center;
+  border: 1px solid #ccc;
+}
+
+.table th {
+  background-color: #f0f0f0;
+  font-weight: bold;
+}
+
+.delete-button {
+  background-color: #ff4d4d;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 6px 10px;
+  cursor: pointer;
+}
+
+.delete-button:hover {
+  background-color: #ff0000;
+}
 </style>
